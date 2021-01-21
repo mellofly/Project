4da229915f5b51be4161a40e679476b8e15f72f1
mellofly <mellofly@outlook.com>
2021-01-21 19:54:15 +0800

    update

--- mybatis/src/main/java/com/it/mapper/AccountMapper.java
+++ mybatis/src/main/java/com/it/mapper/AccountMapper.java
@@ -2,6 +2,23 @@ package com.it.mapper;
 
 import com.it.po.Account;
 
+import java.util.List;
+
 public interface AccountMapper {
+
+    /**
+     * 根据用户id查询用户信息
+     * @param id
+     * @return 用户信息
+     * @throws Exception
+     */
     public Account findAccountById(int id)throws Exception;
+
+    /**
+     *根据输入查询用户信息
+     * @param account
+     * @return
+     * @throws Exception
+     */
+    public List<Account> findAccountsByMessage(Account account)throws Exception;
 }
--- mybatis/src/test/java/com/it/Test.java
+++ mybatis/src/test/java/com/it/Test.java
@@ -1,5 +1,6 @@
 package com.it;
 
+import com.it.po.Account;
 import com.it.test.demo;
 
 import java.io.IOException;
@@ -7,6 +8,9 @@ import java.io.IOException;
 public class Test {
     public static void main(String[] args) throws Exception {
         demo tes = new demo();
-        tes.findUserByIdTest();
+        //tes.findUserByIdTest();
+        Account account = new Account();
+        account.setName("a");
+        tes.findUserByMessageTest(account);
     }
 }
